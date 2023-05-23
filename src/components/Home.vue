5f76760dc4986ce7beb5c9cf689d7c0eb2ca8090
Ram <126403725+Ram85-git@users.noreply.github.com>
2023-05-23 18:05:58 -0400

    Project added

---
+++ src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <h1>Email {{ email }}</h1>
+    <h1>Mobile {{ mobile }}</h1>
+</template>
+
+<script>
+
+  export default {
+    name :"Home",
+    data()
+    {
+        return {
+            email:"bhatth@123.com",
+            mobile:123456789
+            
+        }
+    }
+  }
+
+
+
+</script>
+
+<style scoped >
+
+h1{
+    color: green;
+}
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,53 @@
+<template>
+<div id="testing">
+   {{name}} Testing
+   <br>
+   <br>
+  <input type="text" v-model="name" />
+  <button v-on:click="clear">Clear</button>
+  <button @click="makeCapital">Caps</button>
+
+</div>
+</template>
+
+<script>
+export default {
+   
+   name: "Test",
+
+   data :() => {
+    return {
+    name: "manish1"
+    };
+   },
+  
+  created: function() {
+    console.log("created");
+  },
+  
+  mounted: function() {
+    console.log("mounted");
+  },
+  updated: function() {
+    console.log("updated");
+
+  },
+  destroyed: function() {
+    console.log("destroyed");
+  },
+
+  
+  methods: {
+    clear: function(){
+        this.name ="";
+    },
+    makeCapital: function(){
+        this.name =this.name.toUpperCase();
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
